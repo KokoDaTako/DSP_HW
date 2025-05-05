3358280e1b6cd4738c5fcb312269e64297bd2e08
KokoDaTako <kunguzov@gmail.com>
2025-05-05 17:26:48 +0300

    [Added] Main function
    Main function doing what is requested in the task

--- HW_Konstantin.cpp
+++ HW_Konstantin.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 #include "Polynomial.h"
-
 using namespace std;
-
 
 int main()
 {
-	double i[4] = { 2,3,4,5 };
-	Polynomial poly(3, i);
-
-
-	cout << poly[2];
-	poly[2] = 10.0;
-	cout << endl << poly[2];
+	Polynomial poly;
 
 	cin >> poly;
-	cout << poly;
 
+	//Coefficients might be reversed depending on how the array is used
+	//In this case coef[0] = coef of x^0, coef[1] = coef of x^1, etc.
+	double coef[4] = { -8, 5, 0, 1 };
+	Polynomial given(3, coef);
+
+	Polynomial multi = poly * given;
+
+	cout << endl << "Multiplied polynomial: " << multi;
+
+	//derivative:
+	--multi;
+	cout << endl << "Derivative: " << multi;
+
+	//Integral:
+	++multi;
+	cout << endl << "Integral: " << multi;
+
+	//Polynomial at x=2
+	double evaluation = multi(2);
 }
